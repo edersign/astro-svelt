87879be07ddaf155bcbce49a1da68396774facfb
Eder Oliveira <edersign@gmail.com>
2026-03-13 19:32:36 +0000

    ajustes no projeto e instalção do stylex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
-import svelte from '@astrojs/svelte';
-import { defineConfig } from 'astro/config';
-import stylex from '@stylexjs/unplugin';
+import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
+import stylex from "@stylexjs/unplugin";
 // https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
-	vite: {
-		plugins: [stylex.vite()],
-	},
+  // Enable Svelte to support Svelte components.
+  integrations: [svelte()],
+  vite: {
+    plugins: [stylex.vite()],
+  },
 });
